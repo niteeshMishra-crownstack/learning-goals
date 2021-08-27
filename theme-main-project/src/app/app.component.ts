013e7a7279659f60b4ef9f11a9b18c27ea72d78e
Mukul kumar <mukul.kumar@crownstack.com>
2021-08-27 11:57:59 +0530

    sidebar added

--- theme-main-project/src/app/app.component.ts
+++ theme-main-project/src/app/app.component.ts
@@ -1,19 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatSidenav) sidenav: MatSidenav
   title = 'theme-main-project';
 
-  showThemeOptions:boolean = false;
+  showThemeOptions: boolean = false;
 
-  themeChange(selectedTheme)
-  {
-     console.log('theme change worked');
+  themeChange(selectedTheme) {
+    console.log('theme change worked');
     document.querySelector('body').setAttribute('theme', selectedTheme);
     this.showThemeOptions = false;
   }
-}
+
+
+
+  constructor(private observer: BreakpointObserver) {
+
+  }
+  ngOnInit() {
+
+
+  }
+
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width:800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close()
+      }
+      else {
+
+        this.sidenav.mode = 'side';
+        this.sidenav.open()
+      }
+    })
+  }
+};
+
+
+
--- theme-main-project/src/app/app.module.ts
+++ theme-main-project/src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatDividerModule } from '@angular/material/divider'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +19,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
