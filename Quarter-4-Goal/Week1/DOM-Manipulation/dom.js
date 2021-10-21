de3e40b997b8f7411a1b765e12b9d2a260cb667e
Mukul kumar <mukul.kumar@crownstack.com>
2021-10-21 09:40:57 +0530

    added untracked file

---
+++ Quarter-4-Goal/Week1/DOM-Manipulation/dom.js
@@ -0,0 +1,25 @@
+//Examine the document object
+// console.dir(document);
+// console.log(document.domain)
+// console.log(document.url)
+// console.log(document.title)
+// document.title= 123; we can change the title of the web page
+// console.log(document.doctype)
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.forms)
+// console.log(document.links)
+// console.log(document.images)
+// console.log(document.all)
+// console.log(document.all[10])
+
+
+//Selectors
+
+var headTag = document.getElementsByTagName('h1')
+headTag.innerText = "Blue"
+console.log(headTag)
+
+
+
+
